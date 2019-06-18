bf2a97b412b4f839052abc72c4fa8fdd18c58248
Jaan <janithsrimal@gmail.com>
2019-06-19 00:05:35 +0530

    2019-06-19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NewsComponent } from './news/news.component';
-import { NewsOneComponent } from './news-one/news-one.component';
+// import { NewsOneComponent } from './news-one/news-one.component';
 import { NewsDetailsComponent } from './news-details/news-details.component';
-import { EventComponent } from './event/event.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { AppointmentComponent } from './appointment/appointment.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/news', pathMatch: 'full' },
   { path: 'news', component: NewsComponent },
-  { path: 'news-one', component: NewsOneComponent },
+  // { path: 'news-one', component: NewsOneComponent },
   { path: 'newsdetail', component: NewsDetailsComponent },
-  { path: 'event', component: EventComponent },
-  { path: 'schedule', component: ScheduleComponent },
-  { path: 'appointment', component: AppointmentComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { NewsComponent } from './news/news.component';
 import { NewsOneComponent } from './news-one/news-one.component';
 import { NewsService } from './news.service';
 import { NewsDetailsComponent } from './news-details/news-details.component';
-import { EventComponent } from './event/event.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { AppointmentComponent } from './appointment/appointment.component';
-import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     NewsOneComponent,
-    NewsDetailsComponent,
-    EventComponent,
-    ScheduleComponent,
-    AppointmentComponent
+    NewsDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [NewsService, ApiService],
+  providers: [NewsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
